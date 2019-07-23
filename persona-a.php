20f81c4694fc11c4b760ab0f1865163b4e94d90d
Erica Tollefson <etollefson@cnm.edu>
2019-07-23 17:03:49 -0600

    commit persona-a and persona-a picture

--- persona-a.php
+++ persona-a.php
@@ -1,1 +1,30 @@
-test
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Signs On 66: Persona A</title>
+	</head>
+
+	<body>
+		<h1>Signs on 66</h1>
+		<h2>Purpose</h2>
+		<p>To enrich the experience of travelers to help them identify historic signs along Route 66 in Albuquerque by creating a simple, interactive website.</p>
+		<h2>Audience</h2>
+		<p>This site is designed for both visitor to Albuquerque and local Albuquerque residents who are curious about the history of Route 66 landmarks.</p>
+		<h2>Goal</h2>
+		<p> The primary goal of this site is to have an interactive website where visitors can get information about locations of Route 66 landmarks, as well as uploading pictures and comments about Route 66 landmarks.</p>
+		<br>
+		<h2>Persona</h2>
+		<img src="persona-a-picture.jpg" alt="picture of traveler" height="350" width="550">
+
+
+		<p><b>Name:</b> Fred Visitor</p>
+		<p><b>Age:</b> 65</p>
+		<p><b>Profession:</b> Fred is a recently retired teacher who now has time to travel to visit family and explore historic landmarks around the U.S.</p>
+		<p><b>Technology:</b> Fred has a basic comfort level with technology, but does not want to spend much time on his phone or computer. He uses an older smartphone, a Windows laptop that is several years old, and uses email, Facebook and texting to communicate with friends and family.</p>
+		<p><b>Attitudes and Behaviors:</b> Fred would rather not deal with technology, but knows that while he is planning his travels he will be using websites frequently to research destination details and purchase plane tickets and reserve hotels.</p>
+		<p><b>Needs:</b> Fred will be traveling to visit family in New Mexico, would like to visit local sites with them, and needs access to easy to navigate, simple and uncluttered websites that will get him the travel information he needs quickly. He will get frustrated and leave a site if he can't quickly find what he is looking for. He also will be traveling primarily with his smartphone, and will need to use sites that are mobile-friendly.</p>
+		<p><b>Goals:</b> Fred will need to use his phone to access information about the places he is visiting with family in New Mexico.</p>
+		<p><b>User Story:</b> As a traveler, I want to easily find information about travel destinations.</p>
+	</body>
+</html>
