c0ba922b1b872b71f3c5630a57cec2ecf543ff8b
Rishita Hariyani <rhariyani@cnm.edu>
2019-07-24 12:00:07 -0600

    Added purpose,goal and audience to website

---
+++ persona-c.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Capstone Project</title>
+	</head>
+
+	<body style="font-size: large">
+
+		<h1>Capstone Project</h1>
+		<h2>SignsOn66</h2>
+		<h2>Purpose</h2>
+		<p>To enrich the experience of travelers to help them identify historic signs along Route 66 in Albuquerque by creating a simple, interactive website.</p>
+		<h2>Audience</h2>
+		<p>This site is designed for both visitor to Albuquerque and local Albuquerque residents who are curious about the history of Route 66 landmarks.</p>
+		<h2>Goal</h2>
+		<p> The primary goal of this site is to have an interactive website where visitors can get information about locations of Route 66 landmarks, as well as uploading pictures and comments about Route 66 landmarks.</p>
+
+		<div>
+			<h2>Persona</h2>
+			<img width="500" src="image1.jpg" alt="route66 photo">
+		</div>
+		<div>
+
+			<p><strong>Name:</strong> Ted Snow  </p>
+
+			<p><strong>Age: </strong>35</p>
+
+			<p><strong>Profession: </strong>Architect and Photographer</p>
+
+			<p><strong>	Technology:</strong> Ted plan to use his camera, laptop and Mobile.</p>
+
+			<p><strong>Attitude and Behaviour</strong></p>
+			<p>Ted is Architect by profession but he his passionate photographer.</p>
+
+			<p><strong>Frustrations and Needs:</strong></p>
+			<p>
+				Ted wants to take pictures of all Route66 sign in city and create photo album for his collection.
+			</p>
+
+			<p><strong>Goals:</strong>Ted wants to share his Route 66 signs photo collection to the website.
+
+			<p><strong>User Story:</strong> As a user,I am new to city and to learn about Route 66 history and view signs around the city.</p>
+
+		</div>
+
+	</body>
+</html>
