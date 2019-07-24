430290a9a1728ca5469cca5cad9db3892a0fc62a
Marsha Battee <marsha@sanesuite.com>
2019-07-24 14:58:50 -0600

    final draft ready for review persona-b file

--- persona-b.php
+++ persona-b.php
@@ -1,3 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"/>
+	<title>Persona B - Jane Wilkes, RN</title>
+</head>
+
+<body>
+
+	<h1>Signs on 66</h1>
+
+	<h2>Purpose</h2>
+		<p>To enrich the experience of travelers to help them identify historic signs along Route 66 in Albuquerque by creating a simple interactive website.</p>
+
+	<h2>Audience</h2>
+		<p>This site is designed for both visitors to Albuquerque and local Albuquerque residents who are curious about the history of Route 66 landmarks.</p>
+
+	<h2>Goal</h2>
+		<p>The primary goal of this site is to have an interactive website where visitors can get information about locations of Route 66 landmarks, as well as uploading pictures and comments about Route 66 landmarks.</p>
+
+	<h2>Demographics - Jane Wilkes, RN</h2>
+		<ul>
+			<li>Age: 34</li>
+			<li>Location: Currently in Albuquerque, NM on a nursing travel assignment.</li>
+			<li>Education: BSN (Nursing), BS (Biology/Chemistry).</li>
+			<li>Job: 2nd career nurse and travel nurse at UNM Hospital emergency department.</li>
+			<li>Family: Single. Closest friends are in DC, whom she met on her last assignment. Grew up in Louisiana, where her family still resides.</li>
+		</ul>
+		<img src="jane.jpg" alt="jane">
+
+	<h2>About</h2>
+		<p>Jane is a nurse with experience in Forensics, ER/Trauma, Burn, and Eating Disorders, among various other specialties. Jane came to Albuquerque to complete a travel nursing assignment that comes to an end next month. She is looking to use her remaining free time to explore parts of Albuquerque that she has yet to see. </p>
+
+		<p>Jane has traveled to 17 countries, mostly alone, and enjoys exploring the culture in the countries she has visited. However, Jane rarely finds time to enjoy local attractions and activities while living in the States. She has often felt that, “there will always be time to do local things.” However, in the cities that Jane has lived in as a travel nurse, taking time out to enjoy her city is rare.</p>
+
+		<p>On Jane’s bucket list is to one day drive the entire route of Hwy 66 from Chicago to L.A. to and explore the many sites along the way. And, while Jane is in Albuquerque, she decides to take a day or two to explore the landmarks on Route 66 within the Albuquerque city limits.</p>
+
+	<h2>Goal</h2>
+		<p>Visit popular destinations along Route 66 Hwy in Albuquerque, NM.</p>
+	<h2>Use of Site</h2>
+		<p>To use the website to find popular landmarks or signs on the Route. Jane will mostly use her smartphone for access to various websites exploring the area.</p>
+	<h2>Tech</h2>
+		<p>Social Network: Has a blog geared towards nurses and travels. Peruses Instagram and Twitter frequently. Posts rarely on Twitter, and occasionally on Instagram.</p>
+	<h2>Frustrations</h2>
+		<p>Lack of time to plan the Route. Jane wants a pre-made guide that could help reduce Jane’s frustration and planning time.</p>
+	<h2>User Story</h2>
+		<p>As a user, Jane will be able to identify Route 66 signs (and popular landmarks) along the Hwy in Albuquerque.</p>
+
+</body>
+</html>
 
 <!DOCTYPE html>
 <html lang="en">
