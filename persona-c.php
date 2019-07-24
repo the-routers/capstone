e5d8d88cf164a0d21ba9775f65badf09d3fa8cde
Rishita Hariyani <rhariyani@cnm.edu>
2019-07-24 14:53:41 -0600

    final draft ready for review persona-c file

--- persona-c.php
+++ persona-c.php
@@ -7,40 +7,44 @@
 
 	<body style="font-size: large">
 
-		<h1>Capstone Project</h1>
-		<h2>SignsOn66</h2>
-		<h2>Purpose</h2>
-		<p>To enrich the experience of travelers to help them identify historic signs along Route 66 in Albuquerque by creating a simple, interactive website.</p>
-		<h2>Audience</h2>
-		<p>This site is designed for both visitor to Albuquerque and local Albuquerque residents who are curious about the history of Route 66 landmarks.</p>
-		<h2>Goal</h2>
-		<p> The primary goal of this site is to have an interactive website where visitors can get information about locations of Route 66 landmarks, as well as uploading pictures and comments about Route 66 landmarks.</p>
+		<h2>Capstone Project</h2>
+		<h3>SignsOn66</h3>
+		<h4>Purpose:</h4>To enrich the experience of travelers to help them identify historic signs along Route 66 in Albuquerque by creating a simple, interactive website.<br>
+		<h4>Audience:</h4>This site is designed for both visitor to Albuquerque and local Albuquerque residents who are curious about the history of Route 66 landmarks.
+		<br>
+		<h4>Goal:</h4>The primary goal of this site is to have an interactive website where visitors can get information about locations of Route 66 landmarks, as well as uploading pictures and comments about Route 66 landmarks.
 
 		<div>
 			<h2>Persona</h2>
-			<img width="500" src="image1.jpg" alt="route66 photo">
+			<img width="300" src="personacapstone.jpg" alt="route66 photographer">
 		</div>
 		<div>
 
 			<p><strong>Name:</strong> Ted Snow  </p>
 
-			<p><strong>Age: </strong>35</p>
+			<p><strong>Age: </strong>35</p> 
 
 			<p><strong>Profession: </strong>Architect and Photographer</p>
 
 			<p><strong>	Technology:</strong> Ted plan to use his camera, laptop and Mobile.</p>
 
-			<p><strong>Attitude and Behaviour</strong></p>
-			<p>Ted is Architect by profession but he his passionate photographer.</p>
-
-			<p><strong>Frustrations and Needs:</strong></p>
-			<p>
-				Ted wants to take pictures of all Route66 sign in city and create photo album for his collection.
+			<p><strong>About Me</strong></p>
+			<p>Ted is new resident to Albuquerque city.<br>
+				Ted is Architect by profession but he his passionate photographer.
 			</p>
 
-			<p><strong>Goals:</strong>Ted wants to share his Route 66 signs photo collection to the website.
+			<p><strong>Frustrations and Needs:</strong></p>
+			<p>Ted wants to visit to Rote66sign location without any surprise that sign is missing.<br>
+				Ted need website which is easily navigates on laptops and mobiles.<br>
+				Ted have time constrains due to busy work schedule.
+			</p>
 
-			<p><strong>User Story:</strong> As a user,I am new to city and to learn about Route 66 history and view signs around the city.</p>
+			<p><strong>Goals:</strong>
+				<p>Ted wants to take pictures of all Route66 sign in city and share his photos with other users through this website.<br>
+				<br>
+			</p>
+
+			<p><strong>User Story:</strong> As a user, I want to explore historic and all other signs of Route66 around the city.</p>
 
 		</div>
 
