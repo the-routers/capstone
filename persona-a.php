e108ad6f724e93dd0fb238393a527aebde766fb7
Erica Tollefson <etollefson@cnm.edu>
2019-07-24 14:14:12 -0600

    update persona-a to fix typo

--- persona-a.php
+++ persona-a.php
@@ -10,7 +10,7 @@
 		<h2>Purpose</h2>
 		<p>To enrich the experience of travelers to help them identify historic signs along Route 66 in Albuquerque by creating a simple, interactive website.</p>
 		<h2>Audience</h2>
-		<p>This site is designed for both visitor to Albuquerque and local Albuquerque residents who are curious about the history of Route 66 landmarks.</p>
+		<p>This site is designed for both visitors to Albuquerque and local Albuquerque residents who are curious about the history of Route 66 landmarks.</p>
 		<h2>Goal</h2>
 		<p> The primary goal of this site is to have an interactive website where visitors can get information about locations of Route 66 landmarks, as well as uploading pictures and comments about Route 66 landmarks.</p>
 		<br>
