833d5c99f73c43bbbcb05fa001e60abe80cffae1
Erica Tollefson <etollefson@cnm.edu>
2019-07-25 20:32:57 -0600

    initial commit of erd.php

---
+++ epic/erd.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Signs on 66:Entity Relationship Diagram</title>
+	</head>
+	<body>
+		<H1>Entity Relationship Diagram</H1>
+
+		<img src="erd.jpg" alt="relationship diagram" height="594" width="1368">
+
+	</body>
+</html>
