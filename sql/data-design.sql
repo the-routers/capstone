22d6b3db7ade248eb28a5d7171d4764b1c017a0d
Rishita Hariyani <rhariyani@cnm.edu>
2019-07-29 14:15:12 -0600

    update changes for persona

---
+++ sql/data-design.sql
@@ -0,0 +1,36 @@
+ALTER DATABASE rhariyani CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+DROP TABLE IF EXISTS sign;
+DROP TABLE IF EXISTS userPhoto;
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE sign(
+   signId BINARY(16) NOT NULL,
+   signDescription VARCHAR(255) NOT NULL ,
+   signLat DECIMAL(10,8) NOT NULL,
+   signLong DECIMAL(11,8) NOT NULL,
+   signName VARCHAR(75) NOT NULL,
+   signType VARCHAR(15) NOT NULL,
+	PRIMARY KEY(signId)
+);
+
+CREATE TABLE user(
+	userId BINARY(16) NOT NULL,
+	userActivationToken CHAR(32) NOT NULL,
+	userEmail VARCHAR(128) NOT NULL,
+	userName VARCHAR(50) NOT NULL,
+	userPassword VARCHAR(50) NOT NULL,
+	PRIMARY KEY(userId)
+);
+
+CREATE TABLE userPhoto(
+	userPhotoId BINARY(16) NOT NULL,
+	userPhotoUserId BINARY(16) NOT NULL,
+	userPhotoSignId BINARY(16) NOT NULL,
+	userPhotoCaption VARCHAR(255) ,
+	userPhotoIsFeature TINYINT(1) NOT NULL ,
+	userPhotoUrl VARCHAR(255) NOT NULL,
+	FOREIGN KEY (userPhotoSignID) REFERENCES sign(signId),
+	FOREIGN KEY (userPhotoUserId) REFERENCES user(userId),
+	PRIMARY KEY(userPhotoId)
+);
