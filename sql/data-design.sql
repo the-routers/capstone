ce425f98d8e9493bf95d568a1315ba712efdf149
Rishita Hariyani <rhariyani@cnm.edu>
2019-07-29 14:41:08 -0600

    Update changes in sql database

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,14 +1,14 @@
 ALTER DATABASE rhariyani CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-DROP TABLE IF EXISTS sign;
 DROP TABLE IF EXISTS userPhoto;
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS sign;
 
 CREATE TABLE sign(
    signId BINARY(16) NOT NULL,
-   signDescription VARCHAR(255) NOT NULL ,
-   signLat DECIMAL(10,8) NOT NULL,
-   signLong DECIMAL(11,8) NOT NULL,
+   signDescription VARCHAR(255) NOT NULL,
+   signLat DECIMAL(9,6) NOT NULL,
+   signLong DECIMAL(9,6) NOT NULL,
    signName VARCHAR(75) NOT NULL,
    signType VARCHAR(15) NOT NULL,
 	PRIMARY KEY(signId)
@@ -20,6 +20,8 @@ CREATE TABLE user(
 	userEmail VARCHAR(128) NOT NULL,
 	userName VARCHAR(50) NOT NULL,
 	userPassword VARCHAR(50) NOT NULL,
+	UNIQUE (userEmail),
+	UNIQUE (userName),
 	PRIMARY KEY(userId)
 );
 
@@ -27,8 +29,8 @@ CREATE TABLE userPhoto(
 	userPhotoId BINARY(16) NOT NULL,
 	userPhotoUserId BINARY(16) NOT NULL,
 	userPhotoSignId BINARY(16) NOT NULL,
-	userPhotoCaption VARCHAR(255) ,
-	userPhotoIsFeature TINYINT(1) NOT NULL ,
+	userPhotoCaption VARCHAR(255),
+	userPhotoIsFeature TINYINT(1) NOT NULL,
 	userPhotoUrl VARCHAR(255) NOT NULL,
 	FOREIGN KEY (userPhotoSignID) REFERENCES sign(signId),
 	FOREIGN KEY (userPhotoUserId) REFERENCES user(userId),
