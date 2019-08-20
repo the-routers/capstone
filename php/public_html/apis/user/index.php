2b832d68e41d7897982f873c889d9436526bc070
Marsha Battee <marsha@sanesuite.com>
2019-08-20 15:08:33 -0600

    created directory structure and file for user api

---
+++ php/public_html/apis/user/index.php
@@ -0,0 +1,17 @@
+<?php
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/Classes/autoload.php";
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
+require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/lib/jwt.php";
+require_once dirname(__DIR__, 3) . "/lib/uuid.php";
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
+
+use TheRouters\Capstone\ {
+	User
+};
+/**
+ * API for User
+ *
+ * @author mbattee (marsha@sanesuite.com)
+ */
