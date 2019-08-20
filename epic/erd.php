96724b2262e29ca5a5f5ca69a5ea4a3dba1b9f9f
Erica Tollefson <etollefson@cnm.edu>
2019-08-19 19:45:03 -0600

    updated erd with change from userPassword to userHash

--- epic/erd.php
+++ epic/erd.php
@@ -7,7 +7,7 @@
 	<body>
 		<H1>Entity Relationship Diagram</H1>
 
-		<img src="erd.jpg" alt="relationship diagram" height="526" width="1634">
+		<img src="erd-08-19.jpg" alt="relationship diagram" height="266" width="814">
 
 	</body>
 </html>
