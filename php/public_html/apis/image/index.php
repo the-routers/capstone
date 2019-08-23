2dded5a8322543295ba0418c317db75f9f57e008
Erica Tollefson <etollefson@cnm.edu>
2019-08-23 11:53:43 -0600

    updated image api after working with Kathleen, Marty

--- php/public_html/apis/image/index.php
+++ php/public_html/apis/image/index.php
@@ -11,6 +11,7 @@ use TheRouters\Capstone\{
 /**
  * Cloudinary API for Images
  *
+ * based on:
  * @author Marty Bonacci
  * @version 1.0
  */
@@ -24,32 +25,35 @@ $reply->status = 200;
 $reply->data = null;
 try {
 	// Grab the MySQL connection
-	$secrets = new \Secrets("/etc/apache2/capstone-mysql/ddctwitter.ini");
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/signson66.ini");
 	$pdo = $secrets->getPdoObject();
 	$cloudinary = $secrets->getSecret("cloudinary");
 	//determine which HTTP method is being used
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
-	$tweetId = filter_input(INPUT_GET, "imageTweetId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$profileId = filter_input(INPUT_GET, "profileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$userPhotoId = filter_input(INPUT_GET, "userPhotoId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$userPhotoSignId = filter_input(INPUT_GET, "userPhotoSignId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$userPhotoUserId = filter_input(INPUT_GET, "userPhotoUserId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$userPhotoCaption = filter_input(INPUT_GET, "userPhotoCaption", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$userPhotoIsFeature = filter_input(INPUT_GET, "userPhotoIsFeature", FILTER_VALIDATE_INT);
 	\Cloudinary::config(["cloud_name" => $cloudinary->cloudName, "api_key" => $cloudinary->apiKey, "api_secret" => $cloudinary->apiSecret]);
 	// process GET requests
 	if($method === "GET") {
 		// set XSRF token
 		setXsrfCookie();
-		$reply->data = Image::getAllImages($pdo)->toArray();
+		$reply->data = UserPhoto::getAllUserPhotos($pdo)->toArray();
 	}  elseif($method === "POST") {
 		//enforce that the end user has a XSRF token.
 		verifyXsrf();
 		//use $_POST super global to grab the needed Id
-		$tweetId = filter_input(INPUT_POST, "tweetId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		$userPhotoSignId = filter_input(INPUT_POST, "userPhotoSignId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 		// assigning variable to the user profile, add image extension
 		$tempUserFileName = $_FILES["image"]["tmp_name"];
 		// upload image to cloudinary and get public id
 		$cloudinaryResult = \Cloudinary\Uploader::upload($tempUserFileName, array("width" => 200, "crop" => "scale"));
 		// after sending the image to Cloudinary, create a new image
-		$image = new Image(generateUuidV4(), $tweetId, $cloudinaryResult["signature"], $cloudinaryResult["secure_url"]);
-		$image->insert($pdo);
+		//todo: add $cloudinaryResult["signature"] here and in table/constructor for userPhoto in order to delete from Cloudinary
+		$userPhoto = new UserPhoto(generateUuidV4(), $userPhotoSignId, $userPhotoUserId, $userPhotoCaption, $userPhotoIsFeature, $cloudinaryResult["secure_url"]);
+		$userPhoto->insert($pdo);
 		// update reply
 		$reply->message = "Image uploaded Ok";
 	}
