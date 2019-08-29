6339f67d884a3e04220c33c3513fda339f75fbae
Erica Tollefson <etollefson@cnm.edu>
2019-08-28 19:47:47 -0600

    update image api per code from Anna

--- php/public_html/apis/image/index.php
+++ php/public_html/apis/image/index.php
@@ -9,58 +9,54 @@ use TheRouters\Capstone\{
 	Sign, User, UserPhoto
 };
 /**
- * Cloudinary API for Images
+ * API for the cloudinary image
  *
- * based on:
- * @author Marty Bonacci
- * @version 1.0
  */
-// start session
-if(session_status() !== PHP_SESSION_ACTIVE) {
+//verify the session start if not active
+if(session_start() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
-// prepare an empty reply
-$reply = new StdClass();
+//prepare an empty reply
+$reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
 try {
-	// Grab the MySQL connection
-	$secrets = new \Secrets("/etc/apache2/capstone-mysql/signson66.ini");
+	$secrets = new ("/etc/apache2/capstone-mysql/Secrets.php");
 	$pdo = $secrets->getPdoObject();
 	$cloudinary = $secrets->getSecret("cloudinary");
 	//determine which HTTP method is being used
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
-	$userPhotoId = filter_input(INPUT_GET, "userPhotoId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$userPhotoSignId = filter_input(INPUT_GET, "userPhotoSignId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$userPhotoUserId = filter_input(INPUT_GET, "userPhotoUserId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$userPhotoCaption = filter_input(INPUT_GET, "userPhotoCaption", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$userPhotoIsFeature = filter_input(INPUT_GET, "userPhotoIsFeature", FILTER_VALIDATE_INT);
-	\Cloudinary::config(["cloud_name" => $cloudinary->cloudName, "api_key" => $cloudinary->apiKey, "api_secret" => $cloudinary->apiSecret]);
-	// process GET requests
+	$userPhotoUserId = filter_input(INPUT_GET, "userPhotoCloudinaryUserId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES); //per Anna: $locationId ; variable_name: locationImageCloudinaryId
+	$userPhotoId =  filter_input( INPUT_GET, "userPhotoId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		//per Anna: $profileId  variable_name: profileId
+	$userPhotoSignId = filter_input(INPUT_GET, "userPhotoSignId", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
+		//per Anna: $id ; variable_name: id
+	\Cloudinary::config(["cloud_name" => $cloudinary->cloudName, "api_key" => $cloudinary->apiKey, "api_secret" =>$cloudinary->apiSecret]);
+	//process GET requests
 	if($method === "GET") {
-		// set XSRF token
+		//set XRRF token
 		setXsrfCookie();
-		$reply->data = UserPhoto::getAllUserPhotos($pdo)->toArray();
-	}  elseif($method === "POST") {
+		$reply->data = UserPhoto:: getAllUserPhotos($pdo)->toArray; //per Anna: Location:: getAllLocations
+	}
+	else if($method === "PUT" || $method === "POST") {
+		// enforce the user has a XSRF token
+		verifyXsrf();
 		//enforce that the end user has a XSRF token.
 		verifyXsrf();
-		//use $_POST super global to grab the needed Id
-		$userPhotoSignId = filter_input(INPUT_POST, "userPhotoSignId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 		// assigning variable to the user profile, add image extension
 		$tempUserFileName = $_FILES["image"]["tmp_name"];
 		// upload image to cloudinary and get public id
 		$cloudinaryResult = \Cloudinary\Uploader::upload($tempUserFileName, array("width" => 200, "crop" => "scale"));
-		// after sending the image to Cloudinary, create a new image
-		//todo: add $cloudinaryResult["signature"] here and in table/constructor for userPhoto in order to delete from Cloudinary
-		$userPhoto = new UserPhoto(generateUuidV4(), $userPhotoSignId, $userPhotoUserId, $userPhotoCaption, $userPhotoIsFeature, $cloudinaryResult["secure_url"]);
-		$userPhoto->insert($pdo);
 		// update reply
-		$reply->message = "Image uploaded Ok";
+		$reply->message = $cloudinaryResult["secure_url"];
 	}
-} catch(Exception $exception) {
+} catch (Exception $exception ) {
 	$reply->status = $exception->getCode();
 	$reply->message = $exception->getMessage();
 }
 header("Content-Type: application/json");
 // encode and return reply to front end caller
 echo json_encode($reply);
+
+
+
