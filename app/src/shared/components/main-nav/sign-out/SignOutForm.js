f51ffb0144bc2034f6b006951a910d0ceef5814c
Marsha Battee <marsha@sanesuite.com>
2019-09-02 11:02:51 -0600

    added links explaining signout component

--- app/src/shared/components/main-nav/sign-out/SignOutForm.js
+++ app/src/shared/components/main-nav/sign-out/SignOutForm.js
@@ -1,7 +1,6 @@
-
-
-
-//This logout work around found at https://github.com/aws-amplify/amplify-js/issues/630
+//This logout work around found at https://github.com/aws-amplify/amplify-js/issues/630. Here is a simple signout button. https://github.com/aws-amplify/amplify-js/pull/1000.
+//Per Paul, wait until upload component page is working before we commit to completing a sign out button.
+//Sign out may automatically occur after some time passes for the user.
 
 
 class App extends React.Component {
