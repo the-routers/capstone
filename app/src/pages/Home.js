08cb228a41bb75608db2fd3f082622b71d3e300d
Marsha Battee <marsha@sanesuite.com>
2019-09-02 11:17:51 -0600

    added new content to home page

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react"
+/* import React from "react"
 
 export const Home = () => {
 	return (
@@ -6,4 +6,27 @@ export const Home = () => {
 			<h1>Home</h1>
 		</>
 	)
-}
+};*/
+
+import React, {useEffect} from 'react';
+import {useSelector, useDispatch} from "react-redux";
+import {getAllTweets} from "../../shared/actions/tweet";
+
+export const Home = () => {
+
+	const tweets = useSelector(state => state.tweets);
+	const dispatch = useDispatch();
+	const test = "testing123";
+	console.log(tweets);
+
+	useEffect((test) => {
+		dispatch(getAllTweets(test));
+	}, [test]);
+
+	return (
+		<>
+			<h3>hello world</h3>
+		</>
+
+	)
+};
