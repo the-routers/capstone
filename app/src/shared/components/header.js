e799906693271b067b16ae04d725da4c3d05d439
Erica Tollefson <etollefson@cnm.edu>
2019-09-03 16:03:25 -0600

    update header component with jumbotron per Marsha

--- app/src/shared/components/header.js
+++ app/src/shared/components/header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import Image from 'react-bootstrap/Image'
+
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -10,20 +12,20 @@ import {MainNav} from "./main-nav/MainNav";
 export const Header = () => {
 	return (
 	<>
-	<Header className=""> //check for alignment or styling needed
-		<Container fluid="true">
-			<Row className="background-img w-100 fluid"> //complete linking/styling to background image
-				<Col sm="6" className="float-left ml-1 ml-sm-2 ml-lg-3 mt-1 mt-sm-2 mt-lg-3">
-					<a href="OUR LOGO" className=""></a> //complete linking to logo
-				</Col>
-				<Col sm="6" className="float-right mr-1 mr-sm-2 mr-lg-3 mt-1 mt-sm-2 mt-lg-3">
-					<li>
-						<MainNav/>
-					</li>
-				</Col>
-			</Row>
-		</Container>
-	</Header>
+		<Jumbotron className="header" fluid> //style header img like marsha's using CSS id/className header-img
+			<Container>
+				<Row className="">
+						<Col sm="6" className="float-left ml-1 ml-sm-2 ml-lg-3 mt-1 mt-sm-2 mt-lg-3">
+							<Img className="logo" src="images/logo.jpg" alt="logo for Signs on 66">
+						</Col>
+						<Col sm="6" className="float-right mr-1 mr-sm-2 mr-lg-3 mt-1 mt-sm-2 mt-lg-3">
+							<div>
+								<MainNav/>
+							</div>
+						</Col>
+				</Row>
+			</Container>
+		</Jumbotron>
 	</>
 )
 };
