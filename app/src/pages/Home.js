3263639d88a566dbf75bb292d518b7dbf2091cf8
Marsha Battee <marsha@sanesuite.com>
2019-09-04 16:27:26 -0600

    created structure for home page map and gallery links

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -2,6 +2,8 @@ import React from "react"
 
 import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import {Footer} from "../shared/components/Footer";
+
 
 
 export const Home = () => {
@@ -10,15 +12,29 @@ export const Home = () => {
 			<Jumbotron fluid id="header-image">
 				<Container>
 					<div className="justify-content-end">
-					<h1>Fluid jumbotron</h1>
-					<p>
-						This is a modified jumbotron that occupies the entire horizontal space of
-						its parent.
-					</p>
+						<h1>Fluid jumbotron</h1>
+						<p>
+							This is a modified jumbotron that occupies the entire horizontal space of
+							its parent.
+						</p>
 					</div>
 				</Container>
 			</Jumbotron>
 
+			<div className="container my-5">
+				<div className="row justify-content-center">
+					<div className="col-6-md map">
+						<img src="https://placekitten.com/500/300" alt=""/>
+					</div>
+					<div className="col-6-md gallery">
+						<img src="https://placekitten.com/500/300" alt=""/>
+					</div>
+				</div>
+			</div>
+
+			<div className="container">
+				<Footer />
+			</div>
 
 		</>
 	)
