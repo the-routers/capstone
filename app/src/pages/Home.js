885ea1089482824848efcbdc2b9b54c511b2b436
Marsha Battee <marsha@sanesuite.com>
2019-09-04 16:53:12 -0600

    added about section to home

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -5,7 +5,6 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 import {Footer} from "../shared/components/Footer";
 
 
-
 export const Home = () => {
 	return (
 		<>
@@ -24,16 +23,25 @@ export const Home = () => {
 			<div className="container my-5">
 				<div className="row justify-content-center">
 					<div className="col-6-md map">
-						<img src="https://placekitten.com/500/300" alt=""/>
+						<img src="https://placekitten.com/500/300" alt="map"/>
 					</div>
 					<div className="col-6-md gallery">
-						<img src="https://placekitten.com/500/300" alt=""/>
+						<img src="https://placekitten.com/500/300" alt="gallery"/>
 					</div>
 				</div>
 			</div>
 
 			<div className="container">
-				<Footer />
+				<h3>About Signs on 66</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+					dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+					ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+					fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+					mollit anim id est laborum.</p>
+			</div>
+
+			<div className="container">
+				<Footer/>
 			</div>
 
 		</>
