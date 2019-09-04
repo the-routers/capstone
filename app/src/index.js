a7a2ed34de9f73771a6a2a0d7eb471185662040c
Rishita Hariyani <rhariyani@cnm.edu>
2019-09-04 14:55:25 -0600

    footer section changes made

--- app/src/index.js
+++ app/src/index.js
@@ -6,6 +6,7 @@ import {Route, Switch} from "react-router";
 import {FourOhFour} from "./pages/FourOhFour";
 import {Home} from "./pages/Home";
 import {Example} from "./pages/Example";
+import {Gallery} from "./pages/Gallery"
 import {PhotoUpload} from "./pages/PhotoUpload";
 import {SignIn} from "./pages/sign-in/SignIn";
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -21,6 +22,7 @@ const Routing = () => (
 			<Switch>
 				<Route exact path="/sign-in" component={SignIn}/>
 				<Route exact path="/example" component={Example}/>
+				<Route exact path="/gallery" component={Gallery}/>
 				<Route exact path="/photo-upload" component={PhotoUpload}/>
 				<Route exact path="/" component={Home}/>
 				<Route component={FourOhFour}/>
--- app/src/shared/components/Footer.js
+++ app/src/shared/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, Route} from "react-router-dom";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -10,25 +10,28 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 export const Footer = () => {
 	return (
 		<>
+			<div className="d-flex justify-content-center">
 			<footer className="page-footer text-muted py-2 py-md-4">
 				<Container fluid="true">
 					<Row>
-						<div className="d-flex justify-content-center">
-							<a href="#" className="d-flex justify-content-center" target="_blank" rel="noopener noreferrer">Suggestion? Email US!</a>
+						<div className="text-center">
+							<a href="mailto:signson66abq@gmail.com" className="text-center" target="_blank" rel="noopener noreferrer"> Suggestion? Email US!
+							</a>
 						</div>
 					</Row>
 					<Row>
-						<div className="d-flex justify-content-center">
-							<a href="#" className="text-center" target="_blank" rel="noopener noreferrer">Home</a>|
-							<a href="#" className="text-center" target="_blank" rel="noopener noreferrer">Find a Sign</a>|
-							<a href="#" className="text-center" target="_blank" rel="noopener noreferrer">Gallery</a>|
-							<a href="#" className="text-center" target="_blank" rel="noopener noreferrer">Post Your Photos </a>|
+						<div className="text-center">
+							<a href="/app/src/pages/Home.js" className="text-center" target="_blank" rel="noopener noreferrer">Home</a>|
+							<a href="/app/src/pages/" className="text-center" target="_blank" rel="noopener noreferrer">Find a Sign</a>|
+							<a href="/app/src/pages/Gallery.js" className="text-center" target="_blank" rel="noopener noreferrer">Gallery</a>|
+							<a href="/app/src/pages/PhotoUpload.js" className="text-center" target="_blank" rel="noopener noreferrer">Post Your Photos </a>|
 							<a href="#" className="text-center" target="_blank" rel="noopener noreferrer">About</a>|
 							<a href="#" className= "text-center" target="_blank" rel="noopener noreferrer">Contact Us</a>
 						</div>
 					</Row>
 				</Container>
 			</footer>
+			</div>
 		</>
 	)
 };
