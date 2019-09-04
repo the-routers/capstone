88967d7466c0a64291af4cbe7e67dcb58520e3be
Rishita Hariyani <rhariyani@cnm.edu>
2019-09-04 10:39:53 -0600

    signin page

--- app/src/index.js
+++ app/src/index.js
@@ -7,14 +7,16 @@ import {FourOhFour} from "./pages/FourOhFour";
 import {Home} from "./pages/Home";
 import {Example} from "./pages/Example";
 import {PhotoUpload} from "./pages/PhotoUpload";
+import {SignIn} from "./pages/sign-in/SignIn";
 
 const Routing = () => (
 	<>
 		<BrowserRouter>
 			<Switch>
-				<Route exact path="/" component={Home}/>
+				<Route exact path="/sign-in" component={SignIn}/>
 				<Route exact path="/example" component={Example}/>
 				<Route exact path="/PhotoUpload" component={PhotoUpload}/>
+				<Route exact path="/" component={Home}/>
 				<Route component={FourOhFour}/>
 			</Switch>
 		</BrowserRouter>
