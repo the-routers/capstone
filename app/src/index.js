130d045a6a1a5ef593629064d71bfe448e8b9523
Erica Tollefson <etollefson@cnm.edu>
2019-09-04 20:28:05 -0600

    updated index.js

--- app/src/index.js
+++ app/src/index.js
@@ -7,23 +7,31 @@ import {Route, Switch} from "react-router";
 import {FourOhFour} from "./pages/FourOhFour";
 import {Home} from "./pages/Home";
 import {Example} from "./pages/Example";
-import {MapComponent} from "./shared/components/MapComponent";
-
-import ReactMapGL from "react-map-gl"
-
-
-import 'bootstrap/dist/css/bootstrap.css';
+import {MapComponent} from "./pages/MapComponent";
+import {PhotoUpload} from "./pages/PhotoUpload";
+import {SignIn} from "./pages/sign-in/SignIn";
 //import 'bootstrap/dist/js/bootstrap.bundle.min';
 import "./index.css";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+	faEnvelope,
+	faKey,
+	faMapMarkerAlt
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faEnvelope, faKey, faMapMarkerAlt);
+
 
 
 const Routing = () => (
 	<>
 		<BrowserRouter>
 			<Switch>
-				<Route exact path="/" component={Home}/>
+				<Route exact path="/sign-in" component={SignIn}/>
 				<Route exact path="/example" component={Example}/>
 				<Route exact path="/map" component={MapComponent}/>
+				<Route exact path="/photo-upload" component={PhotoUpload}/>
+				<Route exact path="/" component={Home}/>
 				<Route component={FourOhFour}/>
 			</Switch>
 		</BrowserRouter>
---
+++ app/src/pages/PhotoUpload.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const PhotoUpload = () => {
+	return (
+		<>
+			<h1>trial</h1>
+		</>
+	)
+};
