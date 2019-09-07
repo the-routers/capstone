c2d5d22da7c5a116a5c51938376996eecc11b9c6
Rishita Hariyani <rhariyani@cnm.edu>
2019-09-07 14:03:07 -0600

    need to add header on gallery and photo page

--- app/src/pages/Gallery.js
+++ app/src/pages/Gallery.js
@@ -27,22 +27,22 @@ export const Gallery =() => {
 				<h2>Gallery</h2>
 				<div className="container">
 					<div  className="row">
-					<div class="box">
-						<div class="boxInner col-2">
+					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+						<div class="boxInner">
 							<img style={{ width: 320, height:320 }} src={clib}/>
-							<div class=" text-justify">Caravan NightClub</div>
+							<div class="titleBox">Caravan NightClub</div>
 						</div>
 					</div>
 
-					<div class="box">
-						<div class="boxInner col-2">
+					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+						<div class="boxInner">
 							<img style={{ width: 320, height:320 }} src={Kimo} />
 							<div class="titleBox">Kimo Theater</div>
 						</div>
 					</div>
 
-					<div class="box">
-						<div class="boxInner col-2">
+					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+						<div class="boxInner">
 							<img style={{ width: 320, height:320 }} src={Nob2} />
 							<div class="titleBox">Nob Hill Court</div>
 						</div>
@@ -51,22 +51,22 @@ export const Gallery =() => {
 					</div>
 
 					<div className="row">
-					<div class="box">
-						<div class="boxInner col-2">
+					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+						<div class="boxInner">
 							<img style={{ width: 320, height:320 }} src={elcamino}/>
 							<div class="titleBox">El Camino Motel</div>
 						</div>
 					</div>
 
-					<div class="box">
-						<div class="boxInner col-2">
+					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+						<div class="boxInner">
 							<img style={{ width: 320, height:320 }} src={lodge} />
 							<div class="titleBox">Luna Loge</div>
 						</div>
 					</div>
 
-					<div class="box">
-						<div class="boxInner col-2">
+					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+						<div class="boxInner">
 							<img style={{ width: 320, height:320 }} src={wall} />
 							<div class="titleBox">Wall</div>
 						</div>
@@ -74,20 +74,20 @@ export const Gallery =() => {
 					</div>
 
 					<div className="row">
-					<div class="box">
-						<div class="boxInner col-2">
+					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+						<div class="boxInner">
 							<img style={{ width: 320, height:320 }} src={elvado} />
 							<div class="titleBox">El Vado Motel</div>
 						</div>
 					</div>
-					<div class="box">
-						<div class="boxInner col-2">
+					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+						<div class="boxInner">
 							<img style={{ width: 320, height:320 }} src={Lindy} />
 							<div class="titleBox">Lindy Dinner</div>
 						</div>
 					</div>
-					<div class="box">
-						<div class="boxInner col-2">
+					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+						<div class="boxInner">
 							<img style={{ width: 320, height:320 }} src={coffeeshop} />
 							<div class="titleBox">Coffee Shop</div>
 						</div>
@@ -95,21 +95,21 @@ export const Gallery =() => {
 					</div>
 
 					<div className="row">
-					<div class="box">
-						<div class="boxInner col-2">
+					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+						<div class="boxInner">
 							<img style={{ width: 320, height:320 }} src={intersection} />
 							<div class="titleBox">Intersection</div>
 						</div>
 					</div>
-						<div className="box">
-							<div className="boxInner col-2">
+						<div className="box mt-2 ml-2 pl-1 p-2 mr-2">
+							<div className="boxInner">
 								<img style={{width: 320, height: 320}}
 									  src={garcia}/>
-								<div>Garcia's </div>
+								<div>Garcia's Cafes</div>
 							</div>
 						</div>
-						<div className="box">
-							<div className="boxInner col-2">
+						<div className="box mt-2 ml-2 pl-1 p-2 mr-2">
+							<div className="boxInner">
 								<img style={{width: 320, height: 320}}
 									  src={doghouse}/>
 								<div className="titleBox">DogHouse</div>
