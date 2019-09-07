98cd2af896dfb26d0fcb5dd6cee0f1992998372e
Rishita Hariyani <rhariyani@cnm.edu>
2019-09-07 13:43:20 -0600

    add sign images

--- app/src/pages/Gallery.js
+++ app/src/pages/Gallery.js
@@ -1,5 +1,20 @@
 import React from "react";
 import {Footer} from "../shared/components/Footer";
+import clib from "../images/clib.jpeg";
+import Kimo from "../images/Kimo.jpg";
+import Nob2 from "../images/Nob2.jpg";
+import wall from "../images/wall.jpg";
+import Nobhill1 from "../images/Nobhill1.jpg";
+import Lindy from "../images/Lindy.jpg";
+import doghouse from "../images/doghouse.jpg";
+import intersection from "../images/intersection.jpg";
+import garcia from "../images/garcia.jpg";
+import eldon from "../images/eldon.jpg";
+import coffeeshop from "../images/coffee shop.jpeg";
+import elvado from "../images/elvado.png";
+import lodge from "../images/lodge.jpeg"
+import elcamino from "../images/elcamino.jpg";
+
 
 export const Gallery =() => {
 	return (
@@ -14,67 +29,46 @@ export const Gallery =() => {
 					<div  className="row">
 					<div class="box">
 						<div class="boxInner col-2">
-							<img style={{ width: 320, height:320 }} src="http://www.dwuser.com/education/content/creating-responsive-tiled-layout-with-pure-css/images/demo/7.jpg" />
-							<div class="titleBox">Butterfly</div>
+							<img style={{ width: 320, height:320 }} src={clib}/>
+							<div class=" text-justify">Caravan NightClub</div>
 						</div>
 					</div>
 
 					<div class="box">
 						<div class="boxInner col-2">
-							<img style={{ width: 320, height:320 }} src="http://www.dwuser.com/education/content/creating-responsive-tiled-layout-with-pure-css/images/demo/1.jpg" />
-							<div class="titleBox">An old greenhouse</div>
+							<img style={{ width: 320, height:320 }} src={Kimo} />
+							<div class="titleBox">Kimo Theater</div>
 						</div>
 					</div>
 
 					<div class="box">
 						<div class="boxInner col-2">
-							<img style={{ width: 320, height:320 }} src="http://www.dwuser.com/education/content/creating-responsive-tiled-layout-with-pure-css/images/demo/2.jpg" />
-							<div class="titleBox">Purple wildflowers</div>
+							<img style={{ width: 320, height:320 }} src={Nob2} />
+							<div class="titleBox">Nob Hill Court</div>
 						</div>
 					</div>
 
-					</div>
-
-					<div className="row">
-					<div class="box">
-						<div class="boxInner col-2">
-							<img style={{ width: 320, height:320 }} src="http://www.dwuser.com/education/content/creating-responsive-tiled-layout-with-pure-css/images/demo/10.jpg" />
-							<div class="titleBox">Crocus close-up</div>
-						</div>
-					</div>
-
-					<div class="box">
-						<div class="boxInner col-2">
-							<img style={{ width: 320, height:320 }} src="http://www.dwuser.com/education/content/creating-responsive-tiled-layout-with-pure-css/images/demo/5.jpg" />
-							<div class="titleBox">Spring daffodils</div>
-						</div>
-					</div>
-
-					<div class="box">
-						<div class="boxInner col-2">
-							<img style={{ width: 320, height:320 }} src="http://www.dwuser.com/education/content/creating-responsive-tiled-layout-with-pure-css/images/demo/6.jpg" />
-							<div class="titleBox">Iris along the path</div>
-						</div>
-					</div>
 					</div>
 
 					<div className="row">
 					<div class="box">
 						<div class="boxInner col-2">
-							<img style={{ width: 320, height:320 }} src="http://www.dwuser.com/education/content/creating-responsive-tiled-layout-with-pure-css/images/demo/8.jpg" />
-							<div class="titleBox">The garden blueprint</div>
+							<img style={{ width: 320, height:320 }} src={elcamino}/>
+							<div class="titleBox">El Camino Motel</div>
 						</div>
 					</div>
+
 					<div class="box">
 						<div class="boxInner col-2">
-							<img style={{ width: 320, height:320 }} src="http://www.dwuser.com/education/content/creating-responsive-tiled-layout-with-pure-css/images/demo/9.jpg" />
-							<div class="titleBox">The patio</div>
+							<img style={{ width: 320, height:320 }} src={lodge} />
+							<div class="titleBox">Luna Loge</div>
 						</div>
 					</div>
+
 					<div class="box">
 						<div class="boxInner col-2">
-							<img style={{ width: 320, height:320 }} src="http://www.dwuser.com/education/content/creating-responsive-tiled-layout-with-pure-css/images/demo/11.jpg" />
-							<div class="titleBox">Bumble bee collecting nectar</div>
+							<img style={{ width: 320, height:320 }} src={wall} />
+							<div class="titleBox">Wall</div>
 						</div>
 					</div>
 					</div>
@@ -82,22 +76,43 @@ export const Gallery =() => {
 					<div className="row">
 					<div class="box">
 						<div class="boxInner col-2">
-							<img style={{ width: 320, height:320 }} src="http://www.dwuser.com/education/content/creating-responsive-tiled-layout-with-pure-css/images/demo/12.jpg" />
-							<div class="titleBox">Winding garden path</div>
+							<img style={{ width: 320, height:320 }} src={elvado} />
+							<div class="titleBox">El Vado Motel</div>
+						</div>
+					</div>
+					<div class="box">
+						<div class="boxInner col-2">
+							<img style={{ width: 320, height:320 }} src={Lindy} />
+							<div class="titleBox">Lindy Dinner</div>
+						</div>
+					</div>
+					<div class="box">
+						<div class="boxInner col-2">
+							<img style={{ width: 320, height:320 }} src={coffeeshop} />
+							<div class="titleBox">Coffee Shop</div>
+						</div>
+					</div>
+					</div>
+
+					<div className="row">
+					<div class="box">
+						<div class="boxInner col-2">
+							<img style={{ width: 320, height:320 }} src={intersection} />
+							<div class="titleBox">Intersection</div>
 						</div>
 					</div>
 						<div className="box">
 							<div className="boxInner col-2">
 								<img style={{width: 320, height: 320}}
-									  src="http://www.dwuser.com/education/content/creating-responsive-tiled-layout-with-pure-css/images/demo/11.jpg"/>
-								<div className="titleBox">Bumble bee collecting nectar</div>
+									  src={garcia}/>
+								<div>Garcia's </div>
 							</div>
 						</div>
 						<div className="box">
 							<div className="boxInner col-2">
 								<img style={{width: 320, height: 320}}
-									  src="http://www.dwuser.com/education/content/creating-responsive-tiled-layout-with-pure-css/images/demo/11.jpg"/>
-								<div className="titleBox">Bumble bee collecting nectar</div>
+									  src={doghouse}/>
+								<div className="titleBox">DogHouse</div>
 							</div>
 						</div>
 					</div>
