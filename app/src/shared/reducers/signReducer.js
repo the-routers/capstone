e16df39953287ba5c406d43e5aaae175fc4694b0
Rishita Hariyani <rhariyani@cnm.edu>
2019-09-08 11:22:41 -0600

    add util file content and create signreducer

--- app/src/shared/reducers/signReducer.js
+++ app/src/shared/reducers/signReducer.js
@@ -2,6 +2,12 @@ export default (state = [], action) => {
 	switch(action.type) {
 		case "GET_ALL_SIGNS":
 			return action.payload;
+		case "GET_SIGN_BY_SIGNSID":
+			return action.payload;
+		case "GET_SIGNS_BY_SIGNNAME":
+			return action.payload;
+		case "GET_SIGNS_BY_SIGNTYPE":
+			return action.payload;
 		default:
 			return state;
 	}
