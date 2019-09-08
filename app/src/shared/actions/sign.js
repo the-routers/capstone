171be7cdfc103f5f5e15fbccc4e2f0d8a9046eb4
Rishita Hariyani <rhariyani@cnm.edu>
2019-09-08 12:00:57 -0600

    all change made

--- app/src/shared/actions/sign.js
+++ app/src/shared/actions/sign.js
@@ -2,23 +2,23 @@ import {httpConfig} from "../utils/http-config";
 
 export const getAllSigns = (test) => async (dispatch) => {
 	const payload =  await httpConfig.get("/apis/sign/");
-	dispatch({type: "FETCH_SIGNS",payload : payload.data });
+	dispatch({type: "FETCH_GET_ALL_SIGNS",payload : payload.data });
 };
 
 
 export const getSignBySignId = (test) => async (dispatch) => {
 	const payload =  await httpConfig.get("/apis/sign/");
-	dispatch({type: "FETCH_SIGNS",payload : payload.data });
+	dispatch({type: "FETCH_GET_SIGN_BY_SIGNSID",payload : payload.data });
 };
 
 
 export const getSignBySignName = (test) => async (dispatch) => {
 	const payload =  await httpConfig.get("/apis/sign/");
-	dispatch({type: "FETCH_SIGNS",payload : payload.data });
+	dispatch({type: "FETCH_GET_SIGNS_BY_SIGNNAME",payload : payload.data });
 };
 
 export const getSignBySignType = (test) => async (dispatch) => {
 	const payload =  await httpConfig.get("/apis/sign/");
-	dispatch({type: "FETCH_SIGNS",payload : payload.data });
+	dispatch({type: "FETCH_GET_SIGNS_BY_SIGNTYPE",payload : payload.data });
 };
 
--- app/src/shared/actions/userPhoto.js
+++ app/src/shared/actions/userPhoto.js
@@ -2,22 +2,22 @@ import {httpConfig} from "../utils/http-config";
 
 export const getUserPhotoByUserPhotoId = (test) => async (dispatch) => {
 	const payload =  await httpConfig.get("/apis/userPhoto/");
-	dispatch({type: "FETCH_USERPHOTOS",payload : payload.data });
+	dispatch({type: "FETCH_GET_USERPHOTOS_BY_USERPHOTOSID",payload : payload.data });
 };
 
 
 export const getAllUserPhotos = (test) => async (dispatch) => {
 	const payload =  await httpConfig.get("/apis/userPhoto/");
-	dispatch({type: "FETCH_USERPHOTOS",payload : payload.data });
+	dispatch({type: "FETCH_GET_ALL_USERPHOTOS",payload : payload.data });
 };
 
 
 export const getUserPhotoByUserPhotoSignId = (test) => async (dispatch) => {
 	const payload =  await httpConfig.get("/apis/userPhoto/");
-	dispatch({type: "FETCH_USERPHOTOS",payload : payload.data });
+	dispatch({type: "FETCH_GET_USERPHOTOS_BY_USERPHOTOSSIGNSID",payload : payload.data });
 };
 
 export const getUserPhotoByUserPhotoUserId = (test) => async (dispatch) => {
 	const payload =  await httpConfig.get("/apis/userPhoto/");
-	dispatch({type: "FETCH_USERPHOTOS",payload : payload.data });
+	dispatch({type: "FETCH_GET_USERPHOTOS_BY_USERPHOTOUSERID",payload : payload.data });
 };
