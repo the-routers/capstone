c59a582548497f76196c26c61200805b1a7abf11
Rishita Hariyani <rhariyani@cnm.edu>
2019-09-09 09:53:55 -0600

    deleted setproxy.js

--- app/src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const proxy = require('http-proxy-middleware');
-module.exports = function(app) {
-	app.use(proxy('/apis', {
-		logLevel: 'debug',
-		target: "https://bootcamp-coders.cnm.edu/~etollefson/capstone/php/public_html/",
-		changeOrigin: true,
-		secure: true, }));
-};
