19f00e4152d37b7e4782614cd85550edb03332b5
Rishita Hariyani <rhariyani@cnm.edu>
2019-09-10 08:29:20 -0600

    changes done

--- app/src/pages/Gallery.js
+++ app/src/pages/Gallery.js
@@ -23,102 +23,105 @@ export const Gallery =() => {
 			<Header/>
 
 			<form className="background-pattern-1">
+				<div className="container">
+					<h2>Gallery</h2>
 					<div className="container">
-				<h2>Gallery</h2>
 						<div  className="row">
-					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
-						<div class="boxInner">
-							<img style={{ width: 320, height:320 }} src={Premiere_Motel}/>
-							<div class="titleBox">Premiere Motel </div>
-						</div>
-					</div>
+							<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+								<div class="boxInner">
+									<img style={{ width: 320, height:320 }} src={Premiere_Motel}/>
+									<div class="titleBox">Premiere-Motek</div>
+								</div>
+							</div>
 
-					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
-						<div class="boxInner">
-							<img style={{ width: 320, height:320 }} src={Kimo} />
-							<div class="titleBox">Kimo Theater</div>
-						</div>
-					</div>
+							<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+								<div class="boxInner">
+									<img style={{ width: 320, height:320 }} src={Kimo} />
+									<div class="titleBox">Kimo Theater</div>
+								</div>
+							</div>
 
-					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
-						<div class="boxInner">
-							<img style={{ width: 320, height:320 }} src={Nob2} />
-							<div class="titleBox">Nob Hill Court</div>
-						</div>
-					</div>
-					</div>
+							<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+								<div class="boxInner">
+									<img style={{ width: 320, height:320 }} src={Nob2} />
+									<div class="titleBox">Nob Hill Court</div>
+								</div>
+							</div>
 
-					<div className="row">
-					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
-						<div class="boxInner">
-							<img style={{ width: 320, height:320 }} src={elcamino}/>
-							<div class="titleBox">El Camino Motel</div>
 						</div>
-					</div>
 
-					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
-						<div class="boxInner">
-							<img style={{ width: 320, height:320 }} src={Nobhill1} />
-							<div class="titleBox">Nob Hill</div>
-						</div>
-					</div>
+						<div className="row">
+							<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+								<div class="boxInner">
+									<img style={{ width: 320, height:320 }} src={elcamino}/>
+									<div class="titleBox">El Camino Motel</div>
+								</div>
+							</div>
 
-					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
-						<div class="boxInner">
-							<img style={{ width: 320, height:320 }} src={wall} />
-							<div class="titleBox">Wall</div>
-						</div>
-					</div>
-					</div>
+							<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+								<div class="boxInner">
+									<img style={{ width: 320, height:320 }} src={lodge} />
+									<div class="titleBox">Luna Loge</div>
+								</div>
+							</div>
 
-					<div className="row">
-					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
-						<div class="boxInner">
-							<img style={{ width: 320, height:320 }} src={elvado} />
-							<div class="titleBox">El Vado Motel</div>
-						</div>
-					</div>
-					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
-						<div class="boxInner">
-							<img style={{ width: 320, height:320 }} src={Lindy} />
-							<div class="titleBox">Lindy Dinner</div>
-						</div>
-					</div>
-					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
-						<div class="boxInner">
-							<img style={{ width: 320, height:320 }} src={coffeeshop} />
-							<div class="titleBox">Coffee Shop</div>
-						</div>
-					</div>
-					</div>
-
-					<div className="row">
-					<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
-						<div class="boxInner">
-							<img style={{ width: 320, height:320 }} src={intersection} />
-							<div class="titleBox">Intersection</div>
-						</div>
-					</div>
-						<div className="box mt-2 ml-2 pl-1 p-2 mr-2">
-							<div className="boxInner">
-								<img style={{width: 320, height: 320}}
-									  src={garcia}/>
-								<div>Garcia's Cafes</div>
+							<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+								<div class="boxInner">
+									<img style={{ width: 320, height:320 }} src={wall} />
+									<div class="titleBox">Wall</div>
+								</div>
 							</div>
 						</div>
-						<div className="box mt-2 ml-2 pl-1 p-2 mr-2">
-							<div className="boxInner">
-								<img style={{width: 320, height: 320}}
-									  src={doghouse}/>
-								<div className="titleBox">DogHouse</div>
+
+						<div className="row">
+							<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+								<div class="boxInner">
+									<img style={{ width: 320, height:320 }} src={elvado} />
+									<div class="titleBox">El Vado Motel</div>
+								</div>
+							</div>
+							<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+								<div class="boxInner">
+									<img style={{ width: 320, height:320 }} src={Lindy} />
+									<div class="titleBox">Lindy Dinner</div>
+								</div>
+							</div>
+							<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+								<div class="boxInner">
+									<img style={{ width: 320, height:320 }} src={coffeeshop} />
+									<div class="titleBox">Coffee Shop</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="row">
+							<div class="box mt-2 ml-2 pl-1 p-2 mr-2">
+								<div class="boxInner">
+									<img style={{ width: 320, height:320 }} src={intersection} />
+									<div class="titleBox">Intersection</div>
+								</div>
+							</div>
+							<div className="box mt-2 ml-2 pl-1 p-2 mr-2">
+								<div className="boxInner">
+									<img style={{width: 320, height: 320}}
+										  src={garcia}/>
+									<div>Garcia's Cafes</div>
+								</div>
+							</div>
+							<div className="box mt-2 ml-2 pl-1 p-2 mr-2">
+								<div className="boxInner">
+									<img style={{width: 320, height: 320}}
+										  src={doghouse}/>
+									<div className="titleBox">DogHouse</div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
+				<div className="container">
+					<Footer />
+				</div>
 			</form>
-		<div className="container">
-		<Footer />
-		</div>
 		</>
 	)
-};
+}
