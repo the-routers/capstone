d783b4443eb6cef7ce632b7ddab6b16929e271a9
Marsha Battee <marsha@sanesuite.com>
2019-09-11 16:39:06 -0600

    added earl grey to signin page

--- app/src/pages/sign-in/SignIn.js
+++ app/src/pages/sign-in/SignIn.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {SignInForm} from "./sign-in/SignInForm";
 import {Footer} from "../../shared/components/Footer";
 import {SignUpForm} from "./sign-up/SignUpForm";
 import {Header} from "../../shared/components/header";
+import {httpConfig} from "../../shared/utils/http-config";
+
 
 export const SignIn =() => {
+
+	useEffect(() => {
+		httpConfig.get("/apis/earl-grey/")
+	});
+
 	return (
 		<>
 			<Header />
